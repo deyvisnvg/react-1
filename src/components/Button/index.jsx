7dc566c688d3037af0d0dde9237ac0c4566bf06a
deyvis <deyvisnvg@gmail.com>
2024-01-08 20:15:22 -0500

    feat(deyvisnvg): Update style button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,9 +2,9 @@ import classNames from "classnames";
 
 export default function Button({ children, handleClick, isFollowing = false, setIsTextState = () => { } }) {
     const buttonStyle = classNames(
-        "rounded-2xl py-2",
+        "rounded-2xl py-2 text-sm",
         {
-            "border-2 px-10 text-white hover:text-red-600 hover:border-red-600": isFollowing,
+            "border-2 text-white hover:text-red-600 hover:border-red-600 w-40": isFollowing,
             "bg-white px-5 hover:bg-gray-200": !isFollowing,
         }
     )
